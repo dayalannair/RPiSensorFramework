85dee4adbf7379c7006c2b64ca3cebe4f0bf63c9
dayalannair <34541777+dayalannair@users.noreply.github.com>
2021-01-04 20:17:31 +0200

    added tx/rx buffers to work with pigpio spi

--- include/Node.hpp
+++ include/Node.hpp
@@ -1,5 +1,9 @@
 #ifndef NODE_H
 #define NODE_H
+#include <pigpio.h>
+//#include <pthread.h>
+#include <iostream>
+using namespace std;
 
 class Node
 {
@@ -9,6 +13,11 @@ protected:
     int outputPort;
     int inputPort;
     bool isOn = false;
+    
+    int spiHandle;
+    char tx_buffer[8];
+    char rx_buffer[8]; 
+
     int nodeType; 
     /*
     1 - control
@@ -18,7 +27,6 @@ protected:
 
 public:
     typedef int NodeControlHandler(int id, byte ctr, unsigned sz);
-    
     recv_c_handler(int port, NodeControlHandler);
     recv_sd_handler(int port, int data, DataHandler *handler);
     send_sd(int data, unsigned sz);
